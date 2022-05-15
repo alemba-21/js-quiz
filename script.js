f236210f8efb6276f62e1a8e3fd58b61a790b6f1
Alemba Alex <alembakiragu@gmail.com>
2022-05-15 04:40:49 +0300

    working on javascript

--- script.js
+++ script.js
@@ -1,64 +1,37 @@
+// Question data
 
-const quiz = document.getElementById("quiz");
-const answerEls = document.querySelectorAll(".answer");
-const questionEl = document.getElementById("question");
-const a_text = document.getElementById("a_text");
-const b_text = document.getElementById("b_text");
-const c_text = document.getElementById("c_text");
-const d_text = document.getElementById("d_text");
-const submitBtn = document.getElementById("submit");
+var questionsData= [
+  {
+    question: "Which language runs in a web browser?",
+    option : ['Java', 'C', 'Python', 'JavaScript'],
+    correct: "JavaScript",
+  },
+  {
+    question: "What does CSS stand for?",
+    option : ['Central Style Sheets', 'Cascading Style Sheet', 'Cascading Simple Sheets', 'Corrected Sheet Source'],
+    correct: "Cascading Style Sheets",
+  },
+  {
+    question: "What does HTML stand for?",
+    option : ['Hypertext Markup Language', 'Hypertext Markdown Language', 'Hyperloop Machine Language', 'Hypertransform Markdown Language'],
+    correct: "Hypertext Markup Language",
+  },
+  {
+    question: "What year was JavaScript launched?",
+    option : ['1996', '1995', '1994', 'none of the above'],
+    correct: "1995",
+  },
+];
 
-let currentQuiz = 0;
-
-let score= 4;
-
-loadQuiz();
-
-function loadQuiz() {
-  deselectAnswers();
-
-  const currentQuizData = questionsData[currentQuiz];
-
-  questionEl.innerText = currentQuizData.question;
-  a_text.innerText = currentQuizData.a;
-  b_text.innerText = currentQuizData.b;
-  c_text.innerText = currentQuizData.c;
-  d_text.innerText = currentQuizData.d;
-}
-
-function deselectAnswers() {
-  answerEls.forEach((answerEl) => (answerEl.checked = false));
-}
-
-function getSelected() {
-  let answer;
-  answerEls.forEach((answerEl) => {
-    if (answerEl.checked) {
-      answer = answerEl.id;
-    }
-  });
-
-  return answer;
-}
-
-submitBtn.addEventListener("click", () => {
-  const answer = getSelected();
-  // console.log(answer);
-
-  if (answer === questionsData[currentQuiz].correct) {
-    score++;
-  }
-  currentQuiz++;
-
-  if (currentQuiz < questionsData.length) {
-    loadQuiz();
-  } else {
-    quiz.innerHTML = `<h2>Your Score: 
-    <br> 
-    ${score}/${questionsData.length}
-    <h2>
-    
-    <button onclick="location.reload()">Reload</button>
-    `;
-  }
-});
+var question = document.getElementById("question");
+var quizHeader = document.getElementById("quiz-header");
+var scoreCard = document.getElementById("scorecard");
+var option0 = document.getElementById("option0");
+var option1 = document.getElementById("option1");
+var option2 = document.getElementById("option2");
+var option3 = document.getElementById("option3");
+var next = document.querySelectorAll('.Next');
+var points = document.getElementById('score');
+var span = document.querySelectorAll('span');
+var i=0;
+var score= 0;
