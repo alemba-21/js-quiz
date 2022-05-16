35fae2055ad530d688866ecfa7c7f4f0984be078
Alemba Alex <alembakiragu@gmail.com>
2022-05-16 15:32:10 +0300

    working on function to calculate scores

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// Question data
-
-var questionsData= [
+ 
+//Question bank
+var questionBank= [
   {
-    question: "Which language runs in a web browser?",
+    question : "Which language runs in a web browser?",
     option : ['Java', 'C', 'Python', 'JavaScript'],
-    correct: "JavaScript",
+    correct : "JavaScript",
   },
   {
     question: "What does CSS stand for?",
@@ -21,17 +21,71 @@ var questionsData= [
     option : ['1996', '1995', '1994', 'none of the above'],
     correct: "1995",
   },
-];
+]
 
-var question = document.getElementById("question");
-var quizHeader = document.getElementById("quiz-header");
-var scoreCard = document.getElementById("scorecard");
-var option0 = document.getElementById("option0");
-var option1 = document.getElementById("option1");
-var option2 = document.getElementById("option2");
-var option3 = document.getElementById("option3");
-var next = document.querySelectorAll('.Next');
-var points = document.getElementById('score');
-var span = document.querySelectorAll('span');
+var question= document.getElementById('question');
+var quizContainer= document.getElementById('quiz-container');
+var scorecard= document.getElementById('scorecard');
+var option0= document.getElementById('option0');
+var option1= document.getElementById('option1');
+var option2= document.getElementById('option2');
+var option3= document.getElementById('option3');
+var next= document.querySelector('.next');
+var points= document.getElementById('score');
+var span= document.querySelectorAll('span');
 var i=0;
 var score= 0;
+
+//function to display questions
+function displayQuestion(){
+    for(var a=0;a<span.length;a++){
+        span[a].style.background='none';
+    }
+    question.innerHTML= 'Q.'+(i+1)+' '+questionBank[i].question;
+    option0.innerHTML= questionBank[i].option[0];
+    option1.innerHTML= questionBank[i].option[1];
+    option2.innerHTML= questionBank[i].option[2];
+    option3.innerHTML= questionBank[i].option[3];
+    stat.innerHTML= "Question"+' '+(i+1)+' '+'of'+' '+questionBank.length;
+}
+
+//function to calculate scores
+
+//function to display next question
+function nextQuestion(){
+    if(i<questionBank.length-1)
+    {
+        i=i+1;
+        displayQuestion();
+    }
+    else{
+        points.innerHTML= score+ '/'+ questionBank.length;
+        quizContainer.style.display= 'none';
+        scoreboard.style.display= 'block'
+    }
+}
+
+//click events to next button
+next.addEventListener('click',nextQuestion);
+
+//Back to Quiz button event
+function backToQuiz(){
+    location.reload();
+}
+
+//function to check Answers
+function checkAnswer(){
+    var answerBank= document.getElementById('answerBank');
+    var answers= document.getElementById('answers');
+    answerBank.style.display= 'block';
+    scoreboard.style.display= 'none';
+    for(var a=0;a<questionBank.length;a++)
+    {
+        var list= document.createElement('li');
+        list.innerHTML= questionBank[a].answer;
+        answers.appendChild(list);
+    }
+}
+
+
+displayQuestion();
